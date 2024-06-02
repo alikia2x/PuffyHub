6a816533de79c4046c5a4c50ff9b2584098e23dd
Alikia2x <ziwen243@outlook.com>
2024-06-02 15:32:30 +0800

    Initial Commit

---
+++ PuffyHub Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PuffyHub Watch App
+//
+//  Created by Alikia2x on 2024/6/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PuffyHub Watch App/PuffyHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuffyHubApp.swift
+//  PuffyHub Watch App
+//
+//  Created by Alikia2x on 2024/6/2.
+//
+
+import SwiftUI
+
+@main
+struct PuffyHub_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
